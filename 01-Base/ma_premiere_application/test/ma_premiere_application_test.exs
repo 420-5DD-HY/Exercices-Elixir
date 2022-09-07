e105547cdf55b2054093c4e6e3add191792eb943
Stéphane Denis <sdenis@cegepsth.qc.ca>
2022-09-07 13:02:00 -0400

    retrait du premier projet en mode applicatif

--- 01-Base/ma_premiere_application/test/ma_premiere_application_test.exs
+++
@@ -1,8 +0,0 @@
-defmodule MaPremiereApplicationTest do
-  use ExUnit.Case
-  doctest MaPremiereApplication
-
-  test "greets the world" do
-    assert MaPremiereApplication.hello() == :world
-  end
-end
